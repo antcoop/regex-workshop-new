872ceac55c9de743e2edf14c6ec8c7e990e505c7
antcoop <acooper@hireology.com>
2020-11-16 11:02:51 -0600

    changed layout a bit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 var form = document.getElementById('form');
+var submitBtn = document.getElementById("submit-btn");
+var inputs = document.querySelectorAll("input");
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   console.log('SUBMIT');
 });
+
+document.querySelector("body").setAttribute("name", "good-times");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].style.backgroundColor = "lightblue";
+  inputs[i].style.fontSize = "3rem";
+}
+
+form.style.padding = "20px";
+form.style.margin = "20px";
+form.style.border = "4px solid darkgray";
+form.style.backgroundColor = "purple";
+form.style.color = "yellow";
+form.style.fontFamily = "Arial, Helvetica, sans-serif";

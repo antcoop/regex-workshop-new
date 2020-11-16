ec8fb598f59e0a7a484d27b1100b6031121494f4
antcoop <acooper@hireology.com>
2020-11-16 11:28:17 -0600

    added some loops

--- scripts.js
+++ scripts.js
@@ -5,7 +5,15 @@ var inputs = document.querySelectorAll("input");
 form.addEventListener('submit', e => {
   console.log(e, "this is event")
   e.preventDefault();
+
+  console.log(e.target, "TARGET");
+
   console.log('SUBMIT');
+
+  for (var i = 0; i < 3; i++) {
+    console.log(i);
+  }
+
   for (var i = 0; i < inputs.length; i++) {
     console.log(inputs[i].value);
   }
@@ -16,14 +24,18 @@ document.querySelector("body").setAttribute("name", "good-times");
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].style.backgroundColor = "lightblue";
   inputs[i].style.fontSize = "3rem";
+  inputs[i].style.textAlign = "right";
 }
 
 form.style.padding = "20px";
 form.style.margin = "20px";
+form.style.float = "right";
 form.style.border = "4px solid darkgray";
 form.style.backgroundColor = "purple";
 form.style.color = "yellow";
 form.style.fontFamily = "Atlas Typewriter, Arial, Helvetica, sans-serif";
+
+
 
 const array = [1,3,4,5,6,89,44,32];
 
